6c10df4a36fddddb20af3d0056e2da4e9f942a64
KhandujaKunal <73628799+KhandujaKunal@users.noreply.github.com>
2021-09-26 13:10:35 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCKEb9969PTNbITt8dg6KMGbgbJXFq3pEM",
+    authDomain: "clone-ec8a5.firebaseapp.com",
+    projectId: "clone-ec8a5",
+    storageBucket: "clone-ec8a5.appspot.com",
+    messagingSenderId: "178143110407",
+    appId: "1:178143110407:web:eb1db288c5c6bc998ad1a2"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var db = firebase.firestore();
